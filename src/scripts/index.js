29bb84148d7f29128d1d5ea232e60fd05394e01d
yeffasol <yeffasol@mail.ru>
2016-12-28 22:35:01 +0300

    Add index.html, psd makets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,47 @@
-const name = 4;
+(function () {
+  var navMain = document.querySelector('.menu');
+  var navToggle = document.querySelector('.menu__toggle');
+  var menuEnter = document.querySelector('.menu__user');
+  navMain.classList.remove('menu--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('menu--closed')) {
+      navMain.classList.remove('menu--closed');
+      navMain.classList.add('menu--opened');
+      menuEnter.classList.add('menu__user--mobile');
+    } else {
+      navMain.classList.add('menu--closed');
+      navMain.classList.remove('menu--opened');
+      menuEnter.classList.remove('menu__user--mobile');
+    }
+  });
+
+})();
+
+(function ($) {
+  $('.menu__link').each(function () {
+    var location = window.location.href;
+    var link = this.href;
+    if (location == link) {
+      $(this).parent().addClass('menu__item--active');
+      $(this).removeAttr('href');
+    } else {
+      $(this).parents().removeClass('menu__item--active');
+    }
+  });
+}(jQuery));
+
+(function () {
+  var login = document.querySelector('.menu__user');
+  var loginForm = document.querySelector('.popup');
+  var loginClose = document.querySelector('.login__btn--close');
+
+  login.addEventListener('click', function (e) {
+    loginForm.style.display = 'block';
+    e.preventDefault();
+  });
+  loginClose.addEventListener('click', function () {
+    loginForm.style.display = 'none';
+
+  })
+})();
